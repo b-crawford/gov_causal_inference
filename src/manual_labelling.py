3947828ff65a44f9a3f38b5cdac636d1c2917fb0
bcrawford-moj <billy.crawford@justice.gov.uk>
2024-10-15 18:08:58 +0100

    Sample only positives

--- src/manual_labelling.py
+++ src/manual_labelling.py
@@ -1,13 +1,7 @@
-import requests
-from bs4 import BeautifulSoup
-import time
-import numpy as np
 from datetime import datetime
-from tqdm import tqdm
 import pandas as pd
 import argparse
 import utils
-import os
 import textwrap
 
 
@@ -24,7 +18,7 @@ def wrap_tab_print(string, width=50):
 def user_labelling():
 
     options = {
-        "1": "The description suggests the research attempts to answer a causal question.",
+        "1": "The description suggests the research possibly uses causal inference.",
         "0": "There is not enough information present to decide whether the research attempts to answer a causal question.",
         "-1": "The description suggests that no causal inference was done or required.",
     }
@@ -48,9 +42,9 @@ def user_labelling():
             )
 
 
-def main(run_id):
+def main(run_id, only_sample_positives=False):
 
-    run_path = utils.get_run_folder_path(args.run_id)
+    run_path = utils.get_run_folder_path(run_id)
 
     # get data
     manually_labelled_examples = utils.get_manually_labelled_examples(run_path)
@@ -70,6 +64,15 @@ def main(run_id):
         merged = merged.loc[unlabelled_indices].drop(labels=["_merge"], axis=1)
     else:
         merged = descriptions
+
+    # only sample ones which have been predicted as a positive
+    if only_sample_positives:
+        nlp = utils.get_nlp(run_path)
+        nlp = nlp[["project_hash"]].loc[nlp["predicted_label"].isin([0, 1])]
+        merged = merged.merge(nlp, on="project_hash", how="inner")
+        print(
+            f"Only labelling positively predicted examples. There are {len(merged)} of these."
+        )
 
     # randomise the order
     merged = merged.sample(frac=1, replace=False)
@@ -99,7 +102,8 @@ def main(run_id):
         wrap_tab_print(row["details"])
 
         # ask the user to label it
-        row["label"] = user_labelling()
+        code2label = {"1": "possibly", "0": "unclear", "-1": "unlikely"}
+        row["label"] = code2label[user_labelling()]
 
         # save the time
         row["labelled_at"] = datetime.now()
@@ -121,8 +125,11 @@ if __name__ == "__main__":
         default=None,
     )
     parser.add_argument("--rescrape", action="store_true", help="Enable verbose output")
+    parser.add_argument(
+        "--only_positives", action="store_true", help="Only sample predicted positives."
+    )
 
     args = parser.parse_args()
 
     # run main function
-    main(args.run_id)
+    main(args.run_id, args.only_positives)
